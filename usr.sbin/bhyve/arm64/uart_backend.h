8d42b726b0977a36f540aa13419151b7c6922114
Andrei Martin <andrei.cos.martin@gmail.com>
2021-04-15 09:20:56 +0000

    bhyve: arm64: add uart pl011 implementation
    based on CTSRD-CHERI/freebsd-morello

---
+++ usr.sbin/bhyve/arm64/uart_backend.h
@@ -0,0 +1,46 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+ *
+ * Copyright (c) 2013 Neel Natu <neel@freebsd.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY NETAPP, INC ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL NETAPP, INC OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+#ifndef _UART_BACKEND_H_
+#define	_UART_BACKEND_H_
+
+struct uart_backend;
+
+struct uart_backend *uart_backend_alloc(void);
+int uart_backend_open(struct uart_backend *b, const char *opts,
+    void (*func)(int, enum ev_type, void *), void *arg);
+
+void uart_rxfifo_reset(struct uart_backend *b, int size);
+int uart_rxfifo_getchar(struct uart_backend *b);
+int uart_rxfifo_numchars(struct uart_backend *b);
+void uart_rxfifo_drain(struct uart_backend *b, bool loopback);
+bool uart_rxfifo_write(struct uart_backend *b, bool loopback, uint8_t ch);
+
+#endif
